2c51741354ba7a722a9e85c5776764ba5b2afd23
yilei <yilei@amazon.com>
2012-12-14 13:07:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doctorapp::Application.config.secret_token = '5434ea6c75d89048b1a7e24611354a846fb6d0a0e85afbe9314677f4374d9ccb3ef49f9c80b1efff41cd7989435d22e496c59596c47fdd5a267b46fd2dfc76ae'
