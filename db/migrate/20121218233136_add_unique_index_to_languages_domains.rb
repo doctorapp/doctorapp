884efbb5d84777040ab98d24581fc8a7d98e9a52
skyeagle87 <tuxi1987@gmail.com>
2013-01-03 12:57:11 -0800

    commit db/migrate

--- db/migrate/20121218233136_add_unique_index_to_languages_domains.rb
+++ db/migrate/20121218233136_add_unique_index_to_languages_domains.rb
@@ -1,7 +1,7 @@
 class AddUniqueIndexToLanguagesDomains < ActiveRecord::Migration
   def change
-		remove_column :doctors_domains, :language_id
-		add_column :doctors_domains, :domain_id, :integer
+		#remove_column :doctors_domains, :language_id
+		#add_column :doctors_domains, :domain_id, :integer
 		add_index :doctors_languages, [:doctor_id, :language_id], unique: true
 		add_index :doctors_domains, [:doctor_id, :domain_id], unique: true
   end
