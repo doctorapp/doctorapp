7c76b7d2f4cb56a3a33c9b642d34ea11d5dd4dd6
xitu <tuxi1987@gmail.com>
2013-01-03 14:45:34 -0800

    fix merge1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+ 	protect_from_forgery
 	include SessionsHelper  # make the module SessionHelper available in Controllers; it is automatically available in views/session
 
-<<<<<<< HEAD
 	before_filter :signed_in_user, only: [:edit, :upadte, :show, :index] 
 	#before_filter :correct_user, only: [:edit, :update]
 
-	end
-=======
 end
->>>>>>> 1191ca3c380d54cdee90bd2cad3c7c0b08d23f98
+
 
