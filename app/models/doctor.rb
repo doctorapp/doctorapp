4cc2caf5d30e5870984fd672e27d1c04257e778c
yilei <yilei@amazon.com>
2013-01-17 09:37:43 -0800

    getting rid of work_days models; refactored into residence

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,6 @@
 class Doctor < User
 	attr_accessible :language_ids
 	attr_accessible :domain_ids
-	attr_accessible :work_days
 	
 	has_and_belongs_to_many :languages
 	has_and_belongs_to_many :domains
@@ -12,20 +11,21 @@ class Doctor < User
 	has_many :offices, through: :residences, dependent: :destroy
 
 	has_many :vacations, dependent: :destroy
-	has_one :work_days, dependent: :destroy, class_name: "DoctorWorkDay"
+#	has_one :work_days, dependent: :destroy, class_name: "DoctorWorkDay"
 	has_one :calendar_setting, dependent: :destroy
 
 	after_create :create_calendar_setting
-  after_initialize :create_work_days
+	
+	#after_initialize :create_work_days
 
 
 	def create_calendar_setting
 		self.calendar_setting = CalendarSetting.create(:slotMinutes => 30, :firstHour => 8, :minTime => 8, :maxTime => 18, :defaultEventMinutes => 30, :doctor_id => self.id )
 	end
 
-	def create_work_days
-		self.work_days = DoctorWorkDay.create(monday: true, tuesday: true, wednesday: true, thursday: true, friday: true, saturday: false, sunday: false)	unless self.work_days != nil
-	end
+#def create_work_days
+#		self.work_days = DoctorWorkDay.create(monday: true, tuesday: true, wednesday: true, thursday: true, friday: true, saturday: false, sunday: false)	unless self.work_days != nil
+#	end
 
 	def self.search_by_name(search)
 		if search
